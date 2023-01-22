53db5c933c9773d635ce5381c854a6715f3979cb
zanxi <consultantweb25@gmail.com>
2023-01-22 23:04:52 +0300

    fix

---
+++ BlockChainDBPay/TransactionData.h
@@ -0,0 +1,22 @@
+#ifndef TransactionData_h
+#define TransactionData_h
+
+struct TransactionData
+{
+    double amount;
+    std::string senderKey;
+    std::string receiverKey;
+    time_t timestamp;
+    
+    TransactionData(){};
+    
+    TransactionData(double amt, std::string sender, std::string receiver, time_t time)
+    {
+        amount = amt;
+        senderKey = sender;
+        receiverKey = receiver;
+        timestamp = time;
+    };
+};
+
+#endif /* TransactionData_h */
